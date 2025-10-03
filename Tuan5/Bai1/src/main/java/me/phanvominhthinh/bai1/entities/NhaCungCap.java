ce6ef3ebc217e92b0ef81f8a2973024964d87749
MihnThihn <144779025+MihnThihn@users.noreply.github.com>
2025-10-03 09:52:35 +0700

    Add files via upload

---
+++ Tuan5/Bai1/src/main/java/me/phanvominhthinh/bai1/entities/NhaCungCap.java
@@ -0,0 +1,15 @@
+package me.phanvominhthinh.bai1.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class NhaCungCap {
+    private String mancc;
+    private String tennhacc;
+    private String diachi;
+    private String sodienthoai;
+}
